c7d3360a792a64423344188a2d5892db85699c31
thaissthephanye <thaissthephanye@gmail.com>
2025-03-29 10:01:17 -0300

    fist commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Menu Mobile
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+// Fechar menu ao clicar em um link
+document.querySelectorAll(".nav-links a").forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
+});
